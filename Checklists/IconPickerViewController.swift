a4289e3e761fb1a9bbd8b4c6c4b20851fa6ea57d
Ryan H <rhoover002@gmail.com>
2018-05-19 22:14:39 -0500

    Added file for allowing user to pick icons

--- Checklists/IconPickerViewController.swift
+++ Checklists/IconPickerViewController.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2018 fatalerr. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol IconPickerViewControllerDelegate: class {
+    func iconPicker(_ picker: IconPickerViewController, didPick iconName: String)
+}
+
+class IconPickerViewController: UITableViewController {
+    weak var delegate: IconPickerViewControllerDelegate?
+    let icons = ["No Icon", "Appointments", "Chores", "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips"]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return icons.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IconCell", for: indexPath)
+        
+        let iconName = icons[indexPath.row]
+        let cell.textLabel!.text = iconName
+        
+    }
+}
+
+
